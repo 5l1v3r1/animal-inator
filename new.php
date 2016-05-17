8b21e8bb4ac6fadae3e8b5dabcc537576325bd95
William Bohannon <william18bohannon@gmail.com>
2016-05-17 18:39:22 -0500

    Create new names (fixed)
    Issue redirected you back to my personal main website. It's back now

--- new.php
+++ new.php
@@ -28,7 +28,7 @@ if ($_POST["name"]) {
 	<!-- AnimateCSS -->
 	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
 	<!--Import Google Icon Font-->
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<!-- Meta tags for page -->
@@ -52,7 +52,7 @@ if ($_POST["name"]) {
 </div>
 
 <center>
-	Go back <a href="/">home</a>
+	Go back <a href="/animal-inator">home</a>
 </center>
 
 <!-- jQuery Plugins -->
